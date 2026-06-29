79923eca4a945e1f8336c61d2a71b7a5056a1d2c
MPatefield <m.patefield95@hotmail.co.uk>
2026-06-29 18:10:56 +0100

    creation of ProfileCard.jsx

--- practical-activity-one/src/App.jsx
+++ practical-activity-one/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import ProfileCard from './Profilecard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const profiles = [
+    { name: "Matthew Patefield", image: heroImg, jobTitle: "Software Engineer", bio: "Passionate about creating innovative web applications." 
+    },
+    { name: "John Doe", image: heroImg, jobTitle: "Product Manager", bio: "Dedicated to driving product innovation and growth." 
+    },
+    { name: "Jane Smith", image: heroImg, jobTitle: "UX Designer", bio: "Focused on creating user-friendly and visually appealing designs."}
+  ]
   return (
-   <ProfileCard />
+   <div>
+    {profiles.map((profile) => (
+      <ProfileCard key={profile.name} name={profile.name} image={profile.image} jobTitle={profile.jobTitle} bio={profile.bio} />
+    ))}
+   </div>
   )
 }
 
--- practical-activity-one/src/Profilecard.jsx
+++ practical-activity-one/src/Profilecard.jsx
@@ -1,8 +1,11 @@
-function ProfileCard() {
+function ProfileCard({ name, image, jobTitle, bio }) {
     return (
         <div>
             <h1>Profile Card</h1>
-            <p>Matthew Patefield</p>
+            <p>{name}</p>
+            <img src={image} alt={name} />
+            <p>{jobTitle}</p>
+            <p>{bio}</p>
         </div>
     )
 }
