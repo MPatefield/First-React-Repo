33655576a58a9ed4bf51d9b13794d0fc9d74f601
MPatefield <m.patefield95@hotmail.co.uk>
2026-06-29 17:54:28 +0100

    first commit

---
+++ practical-activity-one/src/Profilecard.jsx
@@ -0,0 +1,10 @@
+function ProfileCard() {
+    return (
+        <div>
+            <h1>Profile Card</h1>
+            <p>Matthew Patefield</p>
+        </div>
+    )
+}
+
+export default ProfileCard
